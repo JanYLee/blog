ac90f8dcf9521df0d0ed201df945fbbd6b00c693
JanYLee <694730359@qq.com>
2019-09-25 01:01:41 +0800

    update: 更新js数据类型判断方法

--- JS/data-type.js
+++ JS/data-type.js
@@ -1,99 +1,21 @@
-/**
- * 
- * 如何准确判断JS数据类型？
- * 
- * typeof 无法准确判断复杂数据类型
- * instanceof 无法正确判断基本数据类型
- * 
- * 本文提供两个思路:
- *  1.首先通过typeof判断出是否是复杂数据类型，如果是，再使用instanceof判断
- *  2.定义自己的数据类型
- *  (附测试代码)
- */
+export default class Utils {
+  static isObject(data) {
+    return Object.prototype.toString.call(data) === '[object Object]';
+  }
 
+  static isString(data) {
+    return Object.prototype.toString.call(data) === '[object String]';
+  }
 
-/**
- * isComplex判断是否是复杂数据类型，如果是返回true,否则返回false
- * @param {*} data 需要被判断类型的数据
- */
+  static isNumber(data) {
+    return Object.prototype.toString.call(data) === '[object Number]';
+  }
 
-function isComplex(data) {
-    if (data && (typeof data === 'object' || typeof data === 'function')) {
-        return true;
+  // 返回数据类型
+  static getTag(data) {
+    if (data == null) {
+      return data === undefined ? '[object Undefined]'.slice(8, -1) : '[object Null]'.slice(8, -1);
     }
-    return false;
+    return Object.prototype.toString.call(data).slice(8, -1);
+  }
 }
-
-
-/**
- * 定义自己的基本数据类型
- */
-
-class PrimitiveString {
-    static [Symbol.hasInstance](data) {
-        return typeof data === 'string';
-    }
-}
-
-class PrimitiveNumber {
-    static [Symbol.hasInstance](data) {
-        return typeof data === 'number';
-    }
-}
-
-class PrimitiveUndefined {
-    static [Symbol.hasInstance](data) {
-        return typeof data === 'undefined';
-    }
-}
-
-class PrimitiveBool {
-    static [Symbol.hasInstance](data) {
-        return typeof data === 'boolean';
-    }
-}
-
-class PrimitiveNull {
-    static [Symbol.hasInstance](data) {
-        return data === null;
-    }
-}
-
-class PrimitiveSymbol {
-    static [Symbol.hasInstance](data) {
-        return typeof data === 'symbol';
-    }
-}
-
-
-
-
-/**
- * 测试 
- */
-
-let num = 2;
-console.log(num instanceof PrimitiveNumber);    //true
-console.log('isComplex: ', isComplex(num));
-
-let str = 'Yvette';
-console.log(str instanceof PrimitiveString);    //true
-console.log('isComplex: ', isComplex(str));
-
-let flag = false;
-console.log(flag instanceof PrimitiveBool);     //true
-console.log('isComplex: ', isComplex(flag));
-
-let und = undefined;
-console.log(und instanceof PrimitiveUndefined); //true
-console.log('isComplex: ', isComplex(und));
-
-let nul = null;
-console.log(nul instanceof PrimitiveNull);      //true
-console.log('isComplex: ', isComplex(nul));
-
-let sym = Symbol(10);
-console.log(sym instanceof PrimitiveSymbol);    //true
-console.log('isComplex: ', isComplex(sym));
-
-console.log('isComplex: ', isComplex(isComplex)); //true
